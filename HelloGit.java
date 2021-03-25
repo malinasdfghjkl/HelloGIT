de6e550635f64af0ab9295c75ae9dc7d86bfcbb4
malinasdfghjkl <malin.marques@gmail.com>
2021-03-25 11:45:50 +0100

    goodbye added

--- HelloGit.java
+++ HelloGit.java
@@ -2,5 +2,7 @@ package HelloGIT;
 
 public class HelloGit {
     public static void main (String[] args){
-    System.out.println("Hello GIT");}
+    System.out.println("Hello GIT");
+    System.out.println("Goodbye");
+}
 }
